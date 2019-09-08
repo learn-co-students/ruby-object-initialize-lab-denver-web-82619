5657ec352194af7936cea3ff2973fb857a97343f
github username <github email address>
2019-09-08 03:55:11 -0600

    Solution

---
+++ lib/dog.rb
@@ -0,0 +1,10 @@
+def Class dog
+    attr_reader :name :breed
+
+    def intialize (name, bread = "Mutt")
+        @name = name
+        @breed = breed
+    end  
+    
+
+end
---
+++ lib/person.rb
@@ -0,0 +1,9 @@
+ def Class Person 
+    attr_reader :name
+
+    def intialize (name)
+        @name = name
+    end  
+    
+
+end
